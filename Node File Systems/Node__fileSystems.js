5f055a81ed899b9406a8cc097bb21d358204fa98
jaseew01 <jseewer@twc.com>
2014-10-01 14:52:34 -0400

    Cleaned up document

--- Node File Systems/Node__fileSystems.js
+++ Node File Systems/Node__fileSystems.js
@@ -1,49 +1,53 @@
 var fs = require('fs');
+var path = require('path');
 
-var dir = "C:\Users\John Seewer\Documents\Hanover College Senior Year\Directed Study";
-var filesList = {};
-var fileSizes = {};
+var dir = process.cwd();
+var filesList = [];
+var fileSizes = [];
 var jsonString = "";
 
-var traverse = function(pathname)
-{
+//attempt Counter
+var traverse = function(pathname,callBack){
 	//fs.readdir - returns list of files in directory:
-	fs.readdir(dir, function(err, files)
-	{
+	fs.readdir(pathname, function(err, files){
 		//for each file in directory:
-		files.forEach(function(file)
-		{
-			file = dir + "/" + file;
-			fs.stat(file, function(err, stat)
-			{
+		files.forEach(function(file){
+			//FIX ME
+			file = path.join(pathname,file);
+
+			fs.stat(file, function(err, stat){
 				if (err) throw err;
-        		if (stat && stat.isDirectory())
-        		{
-					traverse(file);
-				}
-				else //object is a directory
-				{
-					filesList.push(file);
-					fileSizes.push(stat["size"]);
-					var data = fs.readFileSync(file,"utf8");
-					console.log(data);
+
+        		if (stat && stat.isDirectory()){
+					traverse(file,callBack);
+				}else{//object is a directory
+					callBack(file,stat["size"]);
+					//filesList.push(file);
+					//fileSizes.push(stat["size"]);
+					fs.readFile(file,"utf8", function(err,data){
+						//console.log(data);
+					});
 				}
 			});
 		});
 	});
 };
 
+traverse(dir,console.log);
+
+
+
+
+
 var writeToJsonFile = function()
 {
-		var json = JSON.stringify(
-		{
+		var json = JSON.stringify({
 			Directory : dir,
 			files : filesList,
 			Sizes : fileSizes
 		});
 
-		fs.writeFile("filesInformation.json",json,function(err)
-		{
+		fs.writeFile("filesInformation.json",json,function(err){
 			if (err) throw err;
 			console.log("Successfully wrote to file");
 		});
