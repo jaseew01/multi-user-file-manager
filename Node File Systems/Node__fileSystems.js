a923474792a0c7404b947ba83b722999c05f69d0
jaseew01 <jseewer@twc.com>
2014-10-01 01:40:48 -0400

    Added more functionality to node File Systems

--- Node File Systems/Node__fileSystems.js
+++ Node File Systems/Node__fileSystems.js
@@ -1,28 +1,50 @@
 var fs = require('fs');
 
-dir = "C:\Users\John Seewer\Documents\Hanover College Senior Year\Directed Study";
-filesList = {};
+var dir = "C:\Users\John Seewer\Documents\Hanover College Senior Year\Directed Study";
+var filesList = {};
+var fileSizes = {};
+var jsonString = "";
 
-function traverse(pathname)
+var traverse = function(pathname)
 {
 	//fs.readdir - returns list of files in directory:
-	fs.readdir(dir, function(err, files))
+	fs.readdir(dir, function(err, files)
 	{
 		//for each file in directory:
-		files.forEach(function(file))
+		files.forEach(function(file)
 		{
 			file = dir + "/" + file;
-			fs.stat(file, function(err, stat) {
+			fs.stat(file, function(err, stat)
+			{
+				if (err) throw err;
         		if (stat && stat.isDirectory())
         		{
 					traverse(file);
 				}
-			else //object is a directory
-			{
-				filesList.push(file);
-				var data = fs.readFileSync(file,"utf8");
-				console.log(data);
-			}
-		}
-	}
-}
+				else //object is a directory
+				{
+					filesList.push(file);
+					fileSizes.push(stat["size"]);
+					var data = fs.readFileSync(file,"utf8");
+					console.log(data);
+				}
+			});
+		});
+	});
+};
+
+var writeToJsonFile = function(
+{
+		var json = JSON.stringify(
+		{
+			"Directory":dir
+			"files":filesList
+			"Sizes":fileSizes
+		});
+
+		fs.writeFile("filesInformation.json",json,function(err)
+		{
+			if (err) throw err;
+			console.log("Successfully wrote to file");
+		});
+});
