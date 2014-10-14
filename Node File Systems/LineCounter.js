6ce152afd08d3ef72343ba0109a8c40769b15bd5
jaseew01 <jseewer@twc.com>
2014-10-13 21:06:46 -0400

    Made changes as discussed on Thursday to Issue #9

--- Node File Systems/LineCounter.js
+++ Node File Systems/LineCounter.js
@@ -1,27 +1,58 @@
 var fs = require('fs');
 var path = require('path');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
 var dir = process.cwd();
 var numLines = 0;
-var jsonObject = {};
 
-function countLines(content,acc,file){
-	content.split('\n').forEach(function(line){
-		if (line != "//" && line != " "){
-			acc += 1;
-		}else{
-			console.log(line);
-		}
-	});
-	jsonObject = {name : file, lines : acc};
-	console.log(jsonObject);
+function FileTraversal(pathname){
+	this.pathname = pathname;
+	this.jsonObjects = [];
+	this.temp = 0;
+};
+
+util.inherits(FileTraversal,EventEmitter);
+FileTraversal.prototype.increment = function increment(){
+	this.temp += 1;
+};
+
+FileTraversal.prototype.addObject = function addObject(item){
+	this.jsonObjects.push(item);
+};
+
+FileTraversal.prototype.decrement = function decrement(){
+	this.temp -= 1;
+	if(this.temp === 0){
+		this.done();
+	}
 }
 
-function processFile(file){
+FileTraversal.prototype.done = function done(){
+	this.emit("done",this.jsonObjects);
+}
+
+FileTraversal.prototype.run = function run(){
+	var that = this;
+
+	//append to json objects to array, add array to jsonObject.
+	function countLines(content,accumulator,file){
+		content.split('\n').forEach(function(line){
+			if (line != "//" && line != " "){
+				accumulator += 1;
+			}else{
+				console.log(line);
+			}
+		});
+		that.addObject({name : file, lines : accumulator});
+	}
+
+	function processFile(file){
+		that.increment();
 		fs.stat(file, function(err, stat){
 			if (err) throw err;
 
-    		if (stat && stat.isDirectory()){
+			if (stat && stat.isDirectory()){
 				traverse(file);
 			}else{
 				if (file.split('.')[1] === 'js'){
@@ -31,15 +62,27 @@ function processFile(file){
 					});
 				}
 			}
+			that.decrement();
 		});
 	}
 
-function traverse(pathname){
+	function traverse(pathname){
+		that.increment();
 		fs.readdir(pathname, function(err, files){
-				files.forEach(function(file){
-					processFile(path.join(pathname,file));
+			files.forEach(function(file){
+				processFile(path.join(pathname,file));
 			});
+			that.decrement();
 		});
 	};
+	processFile(that.pathname);
+}
 
-processFile(dir);
+var myTraverser = new FileTraversal(dir);
+myTraverser.on("done",printJsonObject);
+myTraverser.run();
+
+function printJsonObject (objectArray){
+	var json = JSON.stringify(objectArray, null, "    ");
+	process.stdout.write(json);
+}
