f963453ce863adc3855b727fc4f2f749dbc29c70
jaseew01 <jseewer@twc.com>
2014-10-13 22:13:51 -0400

    Expanded on the previous commit for Issue #10
    mostly just wrote startup code that has not yet been tested.  Wrote a
    function that will get called every 3 hours to check to see if the log
    file has exceeded 1MB, if so, it creates a new log file.  Also created a
    function that gets called every 5 seconds to write some information to
    the log file (which I haven't decided yet).  Lastly, I expanded onto the
    '/readLogFile' request so that it will return all the data contained in
    the current Log file.

--- beginningWebServers.js
+++ beginningWebServers.js
@@ -1,5 +1,8 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
+
+var currLogFile = process.argv[3];
 
 app.use('/', function (req, res, next) {
 	//handle request information
@@ -12,9 +15,32 @@ app.use('/', function (req, res, next) {
   	next();
 });
 
-app.use('/remove', function (req, res, next){
+app.use('/readLogFile', function (req, res, next){
+	fs.readFile(currLogFile, function (err, data){
+		if (err) throw err;
+		res.send(data);
+	})
 	next();
 });
 
-//log file @: process.argv[3]
+function checkLogFileSize(){
+	fs.stat(currLogFile, function (err, stat){
+		if (err) throw err;
+		if(stat['size'] >= 1000000){
+			var temp = currLogFile.split('.');
+			currLogFile = temp[0]+'1'+temp[1];
+		}
+	});
+}
+
+function writeToLogFile(){
+	fs.appendFile(currLogFile,"",function(err){
+		if (err) throw err;
+		console.log("Successfully wrote to file");
+	});
+}
+
+setInterval(writeToLogFile, 5000);
+setInterval(checkLogFileSize, 10800000);
+
 app.listen(process.argv[2]);
