ad4f9012a890df3a0448b13a4c8d53de875a5c64
jaseew01 <jseewer@twc.com>
2014-10-21 22:51:14 -0400

    Finished up work on Issue #10
    Created a config.json file, got the get working that will send back the
    entire log file.  Also added in a 404 not found message, for if the log
    file for some reason is not accessible.

--- beginningWebServers.js
+++ beginningWebServers.js
@@ -2,33 +2,40 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-var currLogFile = process.argv[3];
+var config = fs.readFileSync('serverConfig.json');
+var configObject = JSON.parse(config);
+
+var currLogFile = configObject.loggingFile;
 var infoToLog = [];
 
 app.use('/', function (req, res, next) {
-	//handle request information
 	var reqInfo = [];
 	console.log(req.route);
 	// req.params.forEach(function (item){
 	// 	reqInfo.push(item);
 	// });
-	infoToLog.push({method : req.get('method'), url : req.get('host')+req.originalUrl});
-  	res.status(200).send('ok');
-  	next();
+	if(req.originalUrl === '/'){
+		infoToLog.push({method : req.method, url : req.get('host')+req.originalUrl});
+  		res.status(200).send('ok');
+  	}else{
+  		next();
+  	}
 });
 
 app.use('/readLogFile', function (req, res, next){
 	fs.readFile(currLogFile, function (err, data){
-		if (err)throw err;
-		res.status(200).send(data);
+		if (err){
+			res.status(404).send("File Not Found");
+		}else{
+			res.status(200).send(data);
+		}
 	});
-	next();
 });
 
 function checkLogFileSize(){
 	fs.stat(currLogFile, function (err, stat){
 		if (err) throw err;
-		if(stat['size'] >= 1000000){
+		if(stat['size'] >= configObject.fileSize){
 			var temp = currLogFile.split('.');
 			var fileNum = Number(temp[0][temp[0].length-1]) + 1;
 			currLogFile = temp[0]+fileNum.toString()+temp[1];
@@ -49,6 +56,6 @@ function writeToLogFile(){
 }
 
 setInterval(writeToLogFile, 5000);
-setInterval(checkLogFileSize, 10800000);
+setInterval(checkLogFileSize, configObject.archiveFreq);
 
-app.listen(process.argv[2]);
+app.listen(configObject.portNum);
